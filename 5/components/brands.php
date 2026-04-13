a1e7789c3d1ad4a3137abc2a822fad03f79c64c5
Oluwasegun Adejuwon <ioadejuwon@gmail.com>
2026-04-13 12:53:33 -0300

    Updated the data

--- 5/components/brands.php
+++ 5/components/brands.php
@@ -7,7 +7,7 @@
                 <div
                     class="col text-center border-end border-color-transparent-dark-very-light sm-border-end-0 sm-border-top transition-inner-all pt-45px pb-45px sm-pt-30px sm-pb-30px">
                     <div class="client-box">
-                        <a href="#"><img src="admin/assets/img/pisl-logo.png" alt=""  style="width: 50%;"/></a>
+                        <a href="#"><img src="admin/assets/img/PISL-Logo.png" alt=""  style="width: 50%;"/></a>
                     </div>
                 </div>
                 <!-- end client item -->
@@ -15,7 +15,7 @@
                 <div
                     class="col text-center border-end border-color-transparent-dark-very-light sm-border-end-0 sm-border-top transition-inner-all pt-45px pb-45px sm-pt-30px sm-pb-30px">
                     <div class="client-box">
-                        <a href="#"><img src="admin/assets/img/pisl-logo.png" alt=""  style="width: 50%;"/></a>
+                        <a href="#"><img src="admin/assets/img/PISL-Logo.png" alt=""  style="width: 50%;"/></a>
                     </div>
                 </div>
                 <!-- end client item -->
@@ -23,7 +23,7 @@
                 <div
                     class="col text-center border-end border-color-transparent-dark-very-light sm-border-end-0 sm-border-top transition-inner-all pt-45px pb-45px sm-pt-30px sm-pb-30px">
                     <div class="client-box">
-                        <a href="#"><img src="admin/assets/img/pisl-logo.png" alt=""  style="width: 50%;"/></a>
+                        <a href="#"><img src="admin/assets/img/PISL-Logo.png" alt=""  style="width: 50%;"/></a>
                     </div>
                 </div>
                 <!-- end client item -->
@@ -31,7 +31,7 @@
                 <div
                     class="col text-center border-end border-color-transparent-dark-very-light sm-border-end-0 md-border-top transition-inner-all pt-45px pb-45px sm-pt-30px sm-pb-30px">
                     <div class="client-box">
-                        <a href="#"><img src="admin/assets/img/pisl-logo.png" alt=""  style="width: 50%;"/></a>
+                        <a href="#"><img src="admin/assets/img/PISL-Logo.png" alt=""  style="width: 50%;"/></a>
                     </div>
                 </div>
                 <!-- end client item -->
@@ -39,7 +39,7 @@
                 <div
                     class="col text-center border-end border-color-transparent-dark-very-light sm-border-end-0 md-border-top transition-inner-all pt-45px pb-45px sm-pt-30px sm-pb-30px">
                     <div class="client-box">
-                        <a href="#"><img src="admin/assets/img/pisl-logo.png" alt=""  style="width: 50%;"/></a>
+                        <a href="#"><img src="admin/assets/img/PISL-Logo.png" alt=""  style="width: 50%;"/></a>
                     </div>
                 </div>
                 <!-- end client item -->
@@ -47,7 +47,7 @@
                 <div
                     class="col text-center border-end border-color-transparent-dark-very-light sm-border-end-0 md-border-top transition-inner-all pt-45px pb-45px sm-pt-30px sm-pb-30px">
                     <div class="client-box">
-                        <a href="#"><img src="admin/assets/img/pisl-logo.png" alt=""  style="width: 50%;"/></a>
+                        <a href="#"><img src="admin/assets/img/PISL-Logo.png" alt=""  style="width: 50%;"/></a>
                     </div>
                 </div>
                 <!-- end client item -->
